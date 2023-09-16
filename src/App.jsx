7435c94ae780afc2e3e057b971d9531831c4d701
Noel Mthembu <109946871+noelmthembu@users.noreply.github.com>
2023-09-16 11:30:27 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import './App.css'
-import Login from './pages/Login'
+import Login from './pages/Login';
+import Register from './pages/Register';
+import New_Password from './pages/New-Password';
+import Forgot_Password from './pages/Forgot-Password';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: 'login',
+    element : <Login></Login>
+  },
+  {
+    path: 'register',
+    element : <Register></Register>
+  },
+  {
+    path: 'forgot_password',
+    element : <Forgot_Password></Forgot_Password>
+  },
+  {
+    path: 'new_password',
+    element : <New_Password></New_Password>
+  }
+])
 
 function App() {
-  
-
   return (
     <>
-      <Login />
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
 
-export default App
+export default App;
